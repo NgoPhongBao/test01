0ea874e003ee7179fb702fed6036b0156e72bd83
Bao Ngo <ngophongbao@gmail.com>
2023-08-01 18:03:08 +0700

    Werwe

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
 const btn = document.querySelector("button");
 btn.addEventListener("click", async () => {
   console.log("ok")
-    window.close();
+    // window.close();
   //   console.log(deferredPrompt)
   // deferredPrompt.prompt();
     
 });
+
+function copyText(element) {
+  var range, selection, worked;
+
+  if (document.body.createTextRange) {
+    range = document.body.createTextRange();
+    range.moveToElementText(element);
+    range.select();
+  } else if (window.getSelection) {
+    selection = window.getSelection();        
+    range = document.createRange();
+    range.selectNodeContents(element);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
+  
+  try {
+    document.execCommand('copy');
+    alert('text copied');
+  }
+  catch (err) {
+    alert('unable to copy text');
+  }
+}
