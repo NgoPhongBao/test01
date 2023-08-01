8de496455575be4a61ae82ee114a6a21e69f7f3b
Bao Ngo <ngophongbao@gmail.com>
2023-08-01 13:54:30 +0700

    ok

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btn = document.querySelector("button");
 btn.addEventListener("click", async () => {
+  console.log("ok")
     window.close();
   //   console.log(deferredPrompt)
   // deferredPrompt.prompt();
